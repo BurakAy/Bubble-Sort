f1d580b1c8f48774193c1d8ff0dd5292642f2b23
BurakAy <burak@burak-aydemir.com>
2016-01-15 18:41:02 -0600

    added method that accepts block

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,5 @@
+=begin
+
 def bubble_sort(array)
 	n = array.length
 
@@ -18,4 +20,30 @@ def bubble_sort(array)
 end
 
 bubble_sort([4,3,78,2,0,2])
+
+=end
+
+def bubble_sort_by(array)
+	n = array.length
+
+	loop do
+		swapped = false
+
+		(n - 1).times do |i|
+			if yield(array[i], array[i+1]) > 0
+				array[i], array[i+1] = array[i+1], array[i]
+				swapped = true
+			end
+		end
+
+		break if not swapped
+	end
+
+	puts array.join(", ")
+	
+end
+
+ bubble_sort_by(["hi","hello","hey"]) do |left,right|
+	left.length - right.length
+ end
 
